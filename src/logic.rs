cf5c08bbf9b0658810974b5068d265ab3a5d2a65
Neil Roberts <bpeeluk@yahoo.co.uk>
2023-06-19 13:29:47 +0200

    logic: Don’t allow guessing longer than the word

--- src/logic.rs
+++ src/logic.rs
@@ -6,17 +6,32 @@ pub enum Event {
 
 pub struct Logic {
     word: String,
+    word_length: usize,
     in_progress_guess: String,
     grid_changed_queued: bool,
 }
 
 impl Logic {
     pub fn new() -> Logic {
-        Logic {
-            word: "POTATO".to_string(),
-            in_progress_guess: "P".to_string(),
+        let mut logic = Logic {
+            word: String::new(),
+            word_length: 0,
+            in_progress_guess: String::new(),
             grid_changed_queued: false,
-        }
+        };
+
+        logic.set_word("POTATO");
+
+        logic
+    }
+
+    fn set_word(&mut self, word: &str) {
+        self.word.clear();
+        self.word.push_str(word);
+        self.word_length = word.chars().count();
+        self.in_progress_guess.clear();
+        self.in_progress_guess.push(word.chars().next().unwrap());
+        self.grid_changed_queued = true;
     }
 
     pub fn set_in_progress_guess(&mut self, guess: &str) {
@@ -27,11 +42,15 @@ impl Logic {
 
         let mut added = 1;
 
-        for ch in xsystem::unicode_chars(guess.chars()) {
+        'add_loop: for ch in xsystem::unicode_chars(guess.chars()) {
             for ch in ch.to_uppercase() {
                 if (added > 1 || ch != first_letter) && is_valid_letter(ch) {
                     self.in_progress_guess.push(ch);
                     added += 1;
+
+                    if added >= self.word_length {
+                        break 'add_loop;
+                    }
                 }
             }
         }
