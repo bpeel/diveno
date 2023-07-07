c42ffafe6966919d4210a89033365ef17dd1bb55
Neil Roberts <bpeeluk@yahoo.co.uk>
2023-07-07 15:10:00 +0200

    tombola: Reduce the slope gradient

--- src/game/tombola.rs
+++ src/game/tombola.rs
@@ -65,8 +65,9 @@ const CLAW_MAX: f32 = RADIUS
 pub const WALL_X: f32 = TOMBOLA_EXTENT + BALL_SIZE * 1.01;
 // Y position of the sides of the slope
 const MIDDLE_SLOPE_Y: f32 = -WALL_X;
-pub const RIGHT_SLOPE_Y: f32 = MIDDLE_SLOPE_Y + BALL_SIZE;
-pub const LEFT_SLOPE_Y: f32 = MIDDLE_SLOPE_Y - BALL_SIZE;
+const SLOPE_DIFFERENCE: f32 = BALL_SIZE;
+pub const RIGHT_SLOPE_Y: f32 = MIDDLE_SLOPE_Y + SLOPE_DIFFERENCE / 2.0;
+pub const LEFT_SLOPE_Y: f32 = MIDDLE_SLOPE_Y - SLOPE_DIFFERENCE / 2.0;
 const SLOPE_WIDTH: f32 = BALL_SIZE;
 
 pub struct Ball {
